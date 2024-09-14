7fbc852fd1e6e0083ec0ee636b71e7d3bed85400
hemu2715 <hemang.trivedi1527@gmail.com>
2024-09-14 17:50:57 +0530

    Add files via upload

---
+++ 1. basic logic program/Q-1.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+Q-1) Display This Information using printf
+a. Your Name
+b. Your Birth date
+c. Your Age
+d. Your Address
+*/
+
+int main()
+{
+    printf("NAME- HEMANG TRIVEDI");
+    printf("\nBIRTH DATE- 27TH OCTOBER, 1994");
+    printf("\nAGE- 29  ");
+    printf("\nADDRESS-SHIVANJALI,22,GURUPRASAD SOCIETY,H.J.DOSHI HOSPITAL WEST,RAJKOT.");
+}
